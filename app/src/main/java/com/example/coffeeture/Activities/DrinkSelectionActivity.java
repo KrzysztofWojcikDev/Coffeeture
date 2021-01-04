d3efee77f268390586597513edbbec13a2d62da9
Krzysztof Wójcik <Krzychoasdf@wp.pl>
2021-01-04 19:19:14 +0100

    Add some activity feature
    I added viewmodel classes based on builder pattern.

--- app/src/main/java/com/example/coffeeture/Activities/DrinkSelectionActivity.java
+++ app/src/main/java/com/example/coffeeture/Activities/DrinkSelectionActivity.java
@@ -3,31 +3,195 @@ package com.example.coffeeture.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
+
 import android.os.Bundle;
 
 import com.example.coffeeture.Presenters.PresenterToolbar;
 import com.example.coffeeture.R;
 import com.example.coffeeture.ViewModels.ViewModelToolbar;
 
-public class DrinkSelectionActivity extends AppCompatActivity {
 
 
-    ViewModelToolbar viewModelToolbar;
-    PresenterToolbar presenterToolbar;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+
+import android.view.View;
+import android.widget.Button;
+
+import com.example.coffeeture.DrinkClass.Americano;
+import com.example.coffeeture.DrinkClass.CafeLate;
+import com.example.coffeeture.DrinkClass.Cappuccino;
+import com.example.coffeeture.DrinkClass.Coffee;
+import com.example.coffeeture.DrinkClass.HotWater;
+import com.example.coffeeture.DrinkClass.Latte;
+import com.example.coffeeture.DrinkClass.Milk;
+import com.example.coffeeture.Presenters.PresenterSettingDrink;
+
+
+    public class DrinkSelectionActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+        ViewModelToolbar viewModelToolbar;
+        PresenterSettingDrink<?> presenterSettingDrink;
+        // PresenterToolbar presenterToolbar;
+        Button button_options;
+        Button button_rinse;
+
+        Button button_small_coffee;
+        Button button_coffee;
+        Button button_americano;
+        Button button_cappuccino;
+        Button button_cafe_late;
+        Button button_latte;
+        Button button_milk;
+        Button button_favorite;
+        Button button_hot_water;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_drink_selection);
+
+            viewModelToolbar = ViewModelProviders.of(DrinkSelectionActivity.this).get(ViewModelToolbar.class);
+            viewModelToolbar = new ViewModelToolbar.ToolbarBuilder()
+                    .setHeadline("Choose your drink")
+                    .setVisibilityForBackButton(false)
+                    .setVisibilityForOptionsButton(true)
+                    .setVisibilityForRinseButton(false)
+                    .build();
+
+            presenterToolbar = new PresenterToolbar(viewModelToolbar);
+
+        }
+
+        // presenterToolbar = new PresenterToolbar(viewModelToolbar);
+        button_options = findViewById(R.id.button_options);
+
+
+
+        button_rinse =findViewById(R.id.button_rinse);
+
+        button_small_coffee =
+
+        findViewById(R.id.first_recipe_button);
+
+        button_coffee =
+
+        findViewById(R.id.second_recipe_button);
+
+        button_americano =
+
+        findViewById(R.id.third_recipe_button);
+
+        button_cappuccino =
+
+        findViewById(R.id.fourth_recipe_button);
+
+        button_cafe_late =
+
+        findViewById(R.id.fift_recipe_button);
+
+        button_latte =
+
+        findViewById(R.id.sixth_recipe_button);
+
+        button_milk =
+
+        findViewById(R.id.seventh_recipe_button);
+
+        button_favorite =
+
+        findViewById(R.id.eighth_recipe_button);
+
+        button_hot_water =
+
+        findViewById(R.id.ninth_recipe_button);
+
+        button_options.setOnClickListener(this);
+        button_rinse.setOnClickListener(this);
+        button_coffee.setOnClickListener(this);
+        button_americano.setOnClickListener(this);
+        button_cappuccino.setOnClickListener(this);
+        button_cafe_late.setOnClickListener(this);
+        button_latte.setOnClickListener(this);
+        button_milk.setOnClickListener(this);
+        button_favorite.setOnClickListener(this);
+        button_hot_water.setOnClickListener(this);
+        button_small_coffee.setOnClickListener(this);
+
+    }
+
+    private void activityStarter(PresenterSettingDrink<?> presenterSettingDrink, Intent intent) {
+        intent.putExtra("presenter", presenterSettingDrink);
+        startActivity(intent);
+    }
+
+    @SuppressLint("NonConstantResourceId")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drink_selection);
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.first_recipe_button:
+                Intent intentSmallCoffees = new Intent(DrinkSelectionActivity.this, SmallDrinkSelectionActivity.class);
+                startActivity(intentSmallCoffees);
+                break;
+            case R.id.second_recipe_button:
+                Intent intentCoffee = new Intent(DrinkSelectionActivity.this, SettingDrinkActivity.class);
+                Coffee coffee = new Coffee.Builder().build();
+                presenterSettingDrink = new PresenterSettingDrink<>(coffee);
+                presenterSettingDrink.activityStarter(presenterSettingDrink, intentCoffee);
+                break;
+            case R.id.third_recipe_button:
+                Intent intentAmericano = new Intent(DrinkSelectionActivity.this, SettingDrinkActivity.class);
+                Americano americano = new Americano.Builder().build();
+                presenterSettingDrink = new PresenterSettingDrink<>(americano);
+                activityStarter(presenterSettingDrink, intentAmericano);
+                break;
+            case R.id.fourth_recipe_button:
+                Intent intentCappuccino = new Intent(DrinkSelectionActivity.this, SettingDrinkActivity.class);
+                Cappuccino cappuccino = new Cappuccino.Builder().build();
+                presenterSettingDrink = new PresenterSettingDrink<>(cappuccino);
+                activityStarter(presenterSettingDrink, intentCappuccino);
+                break;
+            case R.id.fift_recipe_button:
+                Intent intentCafeLate = new Intent(DrinkSelectionActivity.this, SettingDrinkActivity.class);
+                CafeLate cafeLate = new CafeLate.Builder().build();
+                presenterSettingDrink = new PresenterSettingDrink<>(cafeLate);
+                activityStarter(presenterSettingDrink, intentCafeLate);
+                break;
+            case R.id.sixth_recipe_button:
+                Intent intentLatte = new Intent(DrinkSelectionActivity.this, SettingDrinkActivity.class);
+                Latte latte = new Latte.Builder().build();
+                presenterSettingDrink = new PresenterSettingDrink<>(latte);
+                activityStarter(presenterSettingDrink, intentLatte);
+                break;
+            case R.id.seventh_recipe_button:
+                Intent intentMilk = new Intent(DrinkSelectionActivity.this, SettingDrinkActivity.class);
+                Milk milk = new Milk.Builder().build();
+                presenterSettingDrink = new PresenterSettingDrink<>(milk);
+                activityStarter(presenterSettingDrink, intentMilk);
+                break;
+            case R.id.eighth_recipe_button:
+                Intent intentFavorite = new Intent(DrinkSelectionActivity.this, SettingDrinkActivity.class);
+                startActivity(intentFavorite);
+                break;
+            case R.id.ninth_recipe_button:
+                Intent intentHotWater = new Intent(DrinkSelectionActivity.this, SettingDrinkActivity.class);
+                HotWater hotWater = new HotWater.Builder().build();
+                presenterSettingDrink = new PresenterSettingDrink<>(hotWater);
+                activityStarter(presenterSettingDrink, intentHotWater);
+                break;
+            case R.id.button_options:
+                Intent intentOptions = new Intent(DrinkSelectionActivity.this, OptionsActivity.class);
+                startActivity(intentOptions);
+                break;
+            case R.id.button_rinse:
+                Intent intentRinse = new Intent(DrinkSelectionActivity.this, RinsingActivity.class);
+                startActivity(intentRinse);
+                break;
 
-        viewModelToolbar = ViewModelProviders.of(DrinkSelectionActivity.this).get(ViewModelToolbar.class);
-        viewModelToolbar = new ViewModelToolbar.ToolbarBuilder()
-                .setHeadline("Choose your drink")
-                .setVisibilityForBackButton(false)
-                .setVisibilityForOptionsButton(true)
-                .setVisibilityForRinseButton(false)
-                .build();
 
-        presenterToolbar = new PresenterToolbar(viewModelToolbar);
+        }
+
 
     }
 
--- app/src/main/java/com/example/coffeeture/ViewModels/ViewModelSettingDrink.java
+++ app/src/main/java/com/example/coffeeture/ViewModels/ViewModelSettingDrink.java
@@ -1,4 +1,208 @@
 package com.example.coffeeture.ViewModels;
 
-public class ViewModelSettingDrink {
+
+import android.widget.ImageView;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.coffeeture.DrinkClass.Drink;
+import com.example.coffeeture.Enums.AromaProfile;
+import com.example.coffeeture.Enums.CupsQuantity;
+import com.example.coffeeture.Enums.TemperatureOfDrink;
+import com.example.coffeeture.Enums.WhatFirst;
+
+public class ViewModelSettingDrink<T extends Drink>  extends ViewModel {
+
+
+    private MutableLiveData<Integer> contentForEditTextAmountOfMilkFoam;
+    private MutableLiveData<Integer> contentForEditTextAmountOfMilk;
+    private MutableLiveData<Integer> contentForEditTextAmountOfCoffee;
+    private MutableLiveData<Integer> contentForEditTextAmountOfHotWater;
+
+    private final LiveData<Boolean> visionForLinearLayoutAmountOfMilkFoam;
+    private final LiveData<Boolean> visionForLinearLayoutAmountOfMilk;
+    private final LiveData<Boolean> visionForLinearLayoutAmountOfCoffee;
+    private final LiveData<Boolean> visionForLinearLayoutAmountOfHotWater;
+
+    private final LiveData<ImageView> visionForMainDrinkImage;
+
+    private final LiveData<Boolean> visionForConstraintLayoutAroma;
+    private final LiveData<Boolean> visionForConstraintLayoutTemperature;
+    private final LiveData<Boolean> visionForConstraintLayoutWhatFirst;
+    private final LiveData<Boolean> visionForConstraintLayoutQuantity;
+
+    private MutableLiveData<Enum> contentForConstraintLayoutAroma;
+    private MutableLiveData<Enum> contentForConstraintLayoutTemperature;
+    private MutableLiveData<Enum> contentForConstraintLayoutWhatFirst;
+    private MutableLiveData<Enum> contentForConstraintLayoutQuantity;
+
+    private final LiveData<Boolean> visionForButtonStartImplementing;
+    private MutableLiveData<Boolean> visionForButtonCreateYourOwn;
+    private final LiveData<Boolean> visionForButtonAddToFavorite;
+
+    /*
+        private final LiveData<Boolean>visionForButtonStartImplementing;
+        MutableLiveData<Boolean>visionForButtonCreateYourOwn;
+
+
+        MutableLiveData<Boolean>visionForButtonAddToFavorite = new MutableLiveData<>();
+        MutableLiveData<Boolean>visionForButtonDeleteFavoriteRecipe= new MutableLiveData<>();
+        MutableLiveData<Boolean>visionForButtonUpdateFavoriteRecipe= new MutableLiveData<>();
+
+    */
+    public ViewModelSettingDrink(T drink) {
+        if (drink.getAmountOfMilkFoam() == 0)
+            this.visionForLinearLayoutAmountOfMilkFoam = new MutableLiveData<>(false);
+        else {
+            this.visionForLinearLayoutAmountOfMilkFoam = new MutableLiveData<>(true);
+            this.contentForEditTextAmountOfMilkFoam = new MutableLiveData<>(drink.getAmountOfMilkFoam());
+        }
+
+        if (drink.getAmountOfMilk() == 0)
+            this.visionForLinearLayoutAmountOfMilk = new MutableLiveData<>(false);
+        else {
+            this.visionForLinearLayoutAmountOfMilk = new MutableLiveData<>(true);
+            this.contentForEditTextAmountOfMilk = new MutableLiveData<>(drink.getAmountOfMilk());
+        }
+
+        if (drink.getAmountOfCoffee() == 0)
+            this.visionForLinearLayoutAmountOfCoffee = new MutableLiveData<>(false);
+        else {
+            this.visionForLinearLayoutAmountOfCoffee = new MutableLiveData<>(true);
+            this.contentForEditTextAmountOfCoffee = new MutableLiveData<>(drink.getAmountOfCoffee());
+        }
+
+        if (drink.getAmountOfHotWater() == 0)
+            this.visionForLinearLayoutAmountOfHotWater = new MutableLiveData<>(false);
+        else {
+            this.visionForLinearLayoutAmountOfHotWater = new MutableLiveData<>(true);
+            this.contentForEditTextAmountOfHotWater = new MutableLiveData<>(drink.getAmountOfHotWater());
+        }
+
+        if (drink.getAromaProfile() == AromaProfile.VOID)
+            this.visionForConstraintLayoutAroma = new MutableLiveData<>(false);
+        else {
+            this.visionForConstraintLayoutAroma = new MutableLiveData<>(true);
+            this.contentForConstraintLayoutAroma = new MutableLiveData<>(drink.getAromaProfile());
+        }
+
+        this.visionForConstraintLayoutTemperature = new MutableLiveData<>(true);
+        this.contentForConstraintLayoutTemperature = new MutableLiveData<>(drink.getTemperatureOfDrink());
+
+
+        if (drink.getAromaProfile() == WhatFirst.VOID)
+            this.visionForConstraintLayoutWhatFirst = new MutableLiveData<>(false);
+        else {
+            this.visionForConstraintLayoutWhatFirst = new MutableLiveData<>(true);
+            this.contentForConstraintLayoutWhatFirst = new MutableLiveData<>(drink.getWhatFirst());
+        }
+
+        if (drink.getAromaProfile() == CupsQuantity.VOID)
+            this.visionForConstraintLayoutQuantity = new MutableLiveData<>(false);
+        else {
+            this.visionForConstraintLayoutQuantity = new MutableLiveData<>(true);
+            this.contentForConstraintLayoutQuantity = new MutableLiveData<>(drink.getCupsQuantity());
+        }
+
+
+    }
 }
+
+    /*
+
+
+    public static class ViewModelSettingDrinkBuilder{
+      private boolean visionForLinearLayoutAmountOfMilkFoam;
+      private boolean visionForLinearLayoutAmountOfMilk;
+      private boolean visionForLinearLayoutAmountOfCoffee;
+      private boolean visionForLinearLayoutAmountOfHotWater;
+      private boolean visionForConstraintLayoutAroma;
+      private boolean visionForConstraintLayoutTemperature;
+      private boolean visionForConstraintLayoutWhatFirst;
+      private boolean visionForConstraintLayoutQuantity;
+
+      private int contentForEditTextAmountOfMilkFoam ;
+      private int contentForEditTextAmountOfMilk;
+      private int contentForEditTextAmountOfCoffee ;
+      private int contentForEditTextAmountOfHotWater ;
+      /*  public ViewModelSettingBuilder(boolean visionForLinearLayoutAmountOfMilkFoam){
+            this.VISION_FOR_LINEAR_LAYOUT_AMOUNT_OF_MILK_FOAM = visionForLinearLayoutAmountOfMilkFoam;
+
+        }
+
+      public ViewModelSettingDrinkBuilder setAmountOfMilkFoamAttributes(int amountOfMilkFoam){
+          if(amountOfMilkFoam == 0)
+              this.visionForLinearLayoutAmountOfMilkFoam = false;
+          else{
+              this.visionForLinearLayoutAmountOfMilkFoam = true;
+              this.contentForEditTextAmountOfMilkFoam = amountOfMilkFoam;
+          }
+          return this;
+      }
+      public ViewModelSettingDrinkBuilder setAmountOfMilkAttributes(int amountOfMilk){
+          if(amountOfMilk == 0)
+              this.visionForLinearLayoutAmountOfMilk= false;
+          else{
+              this.visionForLinearLayoutAmountOfMilk = true;
+              this.contentForEditTextAmountOfMilk = amountOfMilk;
+          }
+          return this;
+      }
+        public ViewModelSettingDrinkBuilder setAmountOfCoffeeAttributes(int amountOfCoffee){
+            if(amountOfCoffee == 0)
+                this.visionForLinearLayoutAmountOfCoffee = false;
+            else{
+                this.visionForLinearLayoutAmountOfCoffee = true;
+                this.contentForEditTextAmountOfCoffee = amountOfCoffee;
+            }
+
+            return this;
+        }
+        public ViewModelSettingDrinkBuilder setAmountOfHotWaterAttributes(int amountOfHotWater){
+            if(amountOfHotWater == 0)
+                this.visionForLinearLayoutAmountOfHotWater = false;
+            else{
+                this.visionForLinearLayoutAmountOfHotWater = true;
+                this.contentForEditTextAmountOfHotWater = amountOfHotWater;
+            }
+            return this;
+        }
+        public ViewModelSettingDrinkBuilder setAromaAttributes(AromaProfile aroma){
+            this.visionForConstraintLayoutAroma = visionForConstraintLayoutAroma;
+            return this;
+        }
+        public ViewModelSettingDrinkBuilder setTemperatureAttributes (TemperatureOfDrink temperature){
+            this.visionForConstraintLayoutTemperature = visionForConstraintLayoutTemperature;
+            return this;
+        }
+        public ViewModelSettingDrinkBuilder setWhatFirstAttributes (WhatFirst WhatFirst){
+            this.visionForConstraintLayoutWhatFirst = visionForConstraintLayoutWhatFirst;
+            return this;
+        }
+        public ViewModelSettingDrinkBuilder setQuantityAttributes (CupsQuantity cupsQuantity){
+            this.visionForConstraintLayoutQuantity = visionForConstraintLayoutQuantity;
+            return this;
+        }
+      public ViewModelSettingDrink build(){
+          return new ViewModelSettingDrink(this);
+      }
+
+
+    }
+    private ViewModelSettingDrink(ViewModelSettingDrinkBuilder builder){
+        //boolean data
+        this.visionForLinearLayoutAmountOfMilkFoam = new MutableLiveData<>(builder.visionForLinearLayoutAmountOfMilkFoam);
+        this.visionForLinearLayoutAmountOfMilk = new MutableLiveData<>(builder.visionForLinearLayoutAmountOfMilk);
+        this.visionForLinearLayoutAmountOfCoffee = new MutableLiveData<>(builder.visionForLinearLayoutAmountOfCoffee);
+        this.visionForLinearLayoutAmountOfHotWater = new MutableLiveData<>(builder.visionForLinearLayoutAmountOfHotWater);
+        this.visionForConstraintLayoutAroma = new MutableLiveData<>(builder.visionForConstraintLayoutAroma);
+        this.visionForConstraintLayoutTemperature = new MutableLiveData<>(builder.visionForConstraintLayoutTemperature);
+        this.visionForConstraintLayoutWhatFirst = new MutableLiveData<>(builder.visionForConstraintLayoutWhatFirst);
+        this.visionForConstraintLayoutQuantity = new MutableLiveData<>(builder.visionForConstraintLayoutQuantity);
+        //int data
+
+
+    }
+*/
